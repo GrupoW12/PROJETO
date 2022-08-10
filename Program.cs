07b1453972839352333497602afaa86b9d10e321
GrupoW12 <projetogit1@gmail.com>
2022-08-09 21:56:48 -0300

    versão top

--- Program.cs
+++ Program.cs
@@ -6,19 +6,27 @@ namespace willian
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----------------------------------------------------------------------------");
-            Console.WriteLine("Digite um número, irei dizer se ele é positivo, negativo ou igual a zero!");
-            Console.WriteLine("----------------------------------------------------------------------------");
-            int number = int.Parse(Console.ReadLine());
-            if(number > 0){
-                Console.WriteLine("O número é maior que zero!");
-            }else if(number < 0){
-                Console.WriteLine("O número é menor que zero!");
-            }else{
-                Console.WriteLine("O número é igual a zero!");
+            int ativador = 1;
+            for(ativador=1; ativador==1;){
+                Console.WriteLine("----------------------------------------------------------------------------");
+                Console.WriteLine("Digite um número, irei dizer se ele é positivo, negativo ou igual a zero!");
+                Console.WriteLine("----------------------------------------------------------------------------");
+                int number = int.Parse(Console.ReadLine());
+                if(number > 0){
+                    Console.WriteLine("O número é maior que zero!");
+                }else if(number < 0){
+                    Console.WriteLine("O número é menor que zero!");
+                }else{
+                    Console.WriteLine("O número é igual a zero!");
+                }
+                Console.WriteLine("----------------------------------------------------------------------------");
+                Console.WriteLine("Deseja joga novamente?");
+                string resposta = Console.ReadLine();
+                if(resposta != "sim"){
+                    ativador = 0;
+                }
+                Console.Clear();
             }
-            Console.WriteLine("----------------------------------------------------------------------------");
-            Console.ReadLine();
         }
     }
 }
