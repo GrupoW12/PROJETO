ac3084f698240d75ad7ed4c5971b8d7b3b9e6877
GrupoW12 <projetogit1@gmail.com>
2022-08-09 21:41:57 -0300

    primeira versão

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace willian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("Digite um número, irei dizer se ele é positivo, negativo ou igual a zero!");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            int number = int.Parse(Console.ReadLine());
+            if(number > 0){
+                Console.WriteLine("O número é maior que zero!");
+            }else if(number < 0){
+                Console.WriteLine("O número é menor que zero!");
+            }else{
+                Console.WriteLine("O número é igual a zero!");
+            }
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+    }
+}
